a714edabc99e2db5944fb103977a14213d86b6d0
Edison Parra <edisonparraortiz8@gmail.com>
2022-09-19 01:52:23 -0500

    updated

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Backend = () => {
   return (
-    <div className="skills__content">
-        <h3 className="skills__title">Backend developer</h3>
-        <div className="skills__box">
-            <div className="skills__group">
-                <div className="skills__data">
-                <i class='bx bx-badge-check' ></i>
-                <div>
-                    <h3 className="skills__name">Nodejs</h3>
-                    <span className="skills__level">Basic</span>
-                </div>
-                </div>
-                <div className="skills__data">
-                <i class='bx bx-badge-check' ></i>
-                <div>
-                    <h3 className="skills__name">Mongo DB</h3>
-                    <span className="skills__level">basic</span>
-                </div>
-                </div>
+    <div className="skills__content-backend">
+      <h3 className="skills__title">Backend developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <div  className="logo-name-n" >
+              <i class="bx bx-badge-check"></i>
+              <h3 className="skills__name">Nodejs</h3>
+              <span className="skills__level-n">Basic</span>
             </div>
+          </div>
+          <div className="skills__data-">
+            <div  className="logo-name-m" >
+              <i class="bx bx-badge-check"></i>
+              <h3 className="skills__name">Mongo DB</h3>
+              <span className="skills__level-m">basic</span>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Backend;
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -2,40 +2,38 @@ import React from "react";
 
 const Frontend = () => {
   return (
-    <div className="skills__content">
+    <div className="skills__content-frontend">
       <h3 className="skills__title">Frontend developer</h3>
 
       <div className="skills__box">
         <div className="skills__group">
           <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
-              <box-icon name="badge-check"></box-icon>
+            <div className="logo-name-h">
+              <i class="bx bx-badge-check"></i>
               <h3 className="skills__name">Html</h3>
-              <span className="skills__level">Intemediate</span>
+              <span className="skills__level-h">Intermediate</span>
             </div>
           </div>
           <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
-              <box-icon name="badge-check"></box-icon>
+            <div className="logo-name-c">
+              <i class="bx bx-badge-check"></i>
               <h3 className="skills__name">Css</h3>
-              <span className="skills__level">Intemediate</span>
+              <span className="skills__level-c">Intermediate</span>
             </div>
           </div>
           <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
+            <div className="logo-name-j">
+              <i class="bx bx-badge-check"></i>
               <h3 className="skills__name">JavaScript</h3>
-              <span className="skills__level">Intemediate</span>
+              <span className="skills__level-j">Intermediate</span>
             </div>
+            
           </div>
           <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
-              <box-icon name="badge-check"></box-icon>
-              <h3 className="skills__name">React js</h3>
-              <span className="skills__level">Intemediate</span>
+            <div className="logo-name-r">
+              <i class="bx bx-badge-check"></i>
+              <h3 className="skills__name-r">React js</h3>
+              <span className="skills__level-r">Intermediate</span>
             </div>
           </div>
         </div>
