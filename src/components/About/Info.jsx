25b7639644ab89797ef0ce3aee250640af1d88cc
Edison Parra <edisonparraortiz8@gmail.com>
2022-09-25 17:06:46 -0500

    changes

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -6,17 +6,17 @@ const Info = () => {
         <div className="about__box">
         <i className="about__icon" class="uil uil-award"></i>
             <h4 className="about__title">Experience</h4>
-            <span className="about__subtitle">1 years working</span>
+            <span className="about__subtitle">1 year <br /> working</span>
         </div>
         <div className="about__box">
         <i  className="about__icon" class="uil uil-briefcase-alt"></i>
             <h4 className="about__title">Completed</h4>
-            <span className="about__subtitle">10 + Projects</span>
+            <span className="about__subtitle">10 +   <br /> Projects</span>
         </div>
         <div className="about__box">
         <i   className="about__icon" class="uil uil-cloud-question"></i>
-            <h4 className="about__title">Support</h4>
-            <span className="about__subtitle">Online 24/7</span>
+            <h4 className="about__title">Education</h4>
+            <span className="about__subtitle">Full-time <br /> Intensive</span>
         </div>
        
     </div>
